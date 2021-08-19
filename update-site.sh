bc1be9516f74e897c0f11d204616e756a6470800
idk <hankhill19580@gmail.com>
2021-08-19 15:47:22 -0400

    minor change to update script

--- update-site.sh
+++ update-site.sh
@@ -3,5 +3,5 @@ dir=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
 cd $dir
 git pull
 make
-rm -f i2pwinupdate.su3
+rm -f i2pwinupdate.su3*
 wget https://github.com/eyedeekay/i2p/releases/download/latest/i2pwinupdate.su3
